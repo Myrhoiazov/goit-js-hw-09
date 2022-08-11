a7b92b472c8b4d51ae98b50af5f04dedca8f1165
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-08-11 17:56:38 +0300

    add: 1 test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+// console.log(getRandomHexColor());
+
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+btnStop.addEventListener('click', hanleClickStopColor);
+btnStart.addEventListener('click', handleClickStart);
+
+btnStop.setAttribute('disabled', true);
+
+
+function handleClickStart() {
+  
+  timerId = setInterval(
+    () => (document.body.style.backgroundColor = getRandomHexColor()),
+    1000
+  );
+
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+}
+
+function hanleClickStopColor() {
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+
+  clearInterval(timerId);
+}
